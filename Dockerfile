911adab00eb587c9e9df095febd93f9b03dc2fba
nkramer44 <noah.ph.kramer@gmail.com>
2020-01-22 16:37:27 -0500

    updated Dockerfile and run script to take simulation arguments

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,8 @@ COPY target/ilp-performance-*.jar /opt/ilp-performance.jar
 COPY runSimulationDocker.sh /opt/runSimulation.sh
 
 ENV SIMULATION=""
-
-CMD /opt/runSimulation.sh ${SIMULATION}
+ENV CONCURRENCY=10
+ENV RAMP_UP=1
+ENV HOLD_FOR=10
+ENV THROUGHPUT=100
+CMD /opt/runSimulation.sh ${SIMULATION} ${CONCURRENCY} ${RAMP_UP} ${HOLD_FOR} ${THROUGHPUT}
--- runSimulationDocker.sh
+++ runSimulationDocker.sh
@@ -5,7 +5,15 @@ JAR_LOCATION=/opt/ilp-performance.jar
 echo Jar location: "$JAR_LOCATION"
 
 simulation_name=$1
+concurrency=$2
+rampUp=$3
+holdFor=$4
+throughput=$5
 
 echo Running simulation: "$simulation_name"
+echo concurrency: "$concurrency"
+echo rampUp: "$rampUp"
+echo holdFor: "$holdFor"
+echo throughput: "$throughput"
 
-java -cp "$JAR_LOCATION" io.gatling.app.Gatling -s "$simulation_name"
+java -Dconcurrency="$concurrency" -Dramp-up="$rampUp" -Dhold-for="$holdFor" -Dthroughput="$throughput" -cp "$JAR_LOCATION" io.gatling.app.Gatling -s "$simulation_name"
